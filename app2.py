37ef7b63603cf0d5a744b09cea6527692a4a82a0
alexandre-cameron-borges <alexandre@ppcetoile.com>
2025-05-12 17:22:02 +0200

    Update app2.py             "CTR prédit":     f"{p_ctr:.2f}%"
                "CTR prédit":     f"{p_ctr:.2f}%"

--- app2.py
+++ app2.py
@@ -55,7 +55,7 @@ if st.button("🚀 Prédire"):
         results.append({
             "Texte":          row["texte"],
             "Classification": label,
-            "CTR prédit":     f"{p_ctr:.2%}"
+            "CTR prédit":     f"{p_ctr:.2f}%"
         })
 
     # DataFrame et conversion CTR en float pour tri
