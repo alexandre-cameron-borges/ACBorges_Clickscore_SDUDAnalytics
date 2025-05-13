5a94c157e50fe3550f0b66c3c7468b57514d71eb
alexandre-cameron-borges <alexandre@ppcetoile.com>
2025-05-14 01:03:47 +0200

    Update app2.py ax.set_yscale("log")

--- app2.py
+++ app2.py
@@ -109,6 +109,8 @@ if st.button("🚀 Prédire"):
 
     fig, ax = plt.subplots(figsize=(5,4))
     ax.scatter(x, df_res["CTR_num"], c=df_res["Classification"].map(color_map), s=300, alpha=0.7, edgecolors="w")
+    ax.set_yscale("log")
+
     # ... après ax.scatter(...)
 # 1) Récupérer min et max
     y_min, y_max = df_res["CTR_num"].min(), df_res["CTR_num"].max()
